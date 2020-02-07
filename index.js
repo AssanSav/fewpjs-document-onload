fd97cff7cf6e98ffd93b96f5c3bba24006cfcfb1
AssanSav <assane.savadogo81@gmail.com>
2020-02-07 07:42:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    updateInnerHtml()
+});
+
+function updateInnerHtml() {
+    let text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+};
